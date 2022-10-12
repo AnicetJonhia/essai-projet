525b21343a98988f5b8331921ceb31379af939dc
AnicetJonhia <113587751+AnicetJonhia@users.noreply.github.com>
2022-10-12 15:14:07 +0300

    Add files via upload

---
+++ Apprendre Css et html/indexjs.js
@@ -0,0 +1,20 @@
+var sexWoman = document.getElementById("sexWoman") , 
+sexMan = document.getElementById("sexMan");  // variable pour les sexes 
+
+var name = document.getElementsByTagName("name"),
+firstName = document.getElementsByTagName("firstName") ; // variable pour les noms et les prénoms
+
+var day = document.getElementsByTagName("day"),
+month = document.getElementsByTagName("month"), 
+year = document.getElementsByTagName("year"); // variables pour la date de naissance
+
+var age = document.getElementsByTagName("age"), 
+mailto = document.getElementsByTagName("mailto"),
+password = document.getElementsByTagName("password"),
+passwordConfirmation = document.getElementsByTagName("passwordConfirmation"); // variable pour les autres id
+
+var nationality = document.getElementsByTagName("nationality"); // variable pour la nationalité.
+
+
+
+
